53dad7e192fb1077db213e11f31d80156bedcfb3
fauziallagan <fauziallagan@gmail.com>
2021-11-30 13:48:02 +0700

    new

---
+++ Tutorial Python/Tahap 1.py
@@ -0,0 +1,46 @@
+"""
+~ Tahap 1 ~
+     pada tahap ini kalian akan belajar tentang syntax dasar pada python Seperti 
+     1. Mencetak Hello World
+     2. Memberikan Komentar
+"""
+#   1. Mencetak Hello World
+print("Hello Wrold")  #Gunakan print() lalu didalam tanda kurung, beri tanda " " atau ' ' untuk mencetak string
+print('Halo Dunia')
+
+#   2. Memberikan Komentar
+#  ini merupakan komentar, komentar ini tidak akan muncul / dieksekusi.
+#   Single Comment
+#  tanda  (#) atau hastag/pagar merupakan single comment atau hanya mencakup 1 baris code
+# Contoh : 
+# print("ini tidak akan dieksekusi oleh python")  #ini tidak akan dicetak / dieksekusi
+print("Ini akan dieksekusi oleh python") #ini akan dicetak /  dieksekusi 
+
+# Multi Comment
+# tanda (""" """) ini merupakan multi comment atau mencakup lebih dari 1 baris kode
+# contoh : 
+"""
+code ini tidak akan dicetak
+print("Line 1")
+print("Line 2")
+print("Line 3")
+
+"""
+
+# challenge 1
+""" 
+    Instruksi 
+- cetak Nama Kalian pada fungsi print() dibawah!
+
+"""
+
+print()
+
+# challenge 2
+""" 
+    Instruksi 
+- Tambahkan komentar agar kode dibawah tidak dijalankan
+
+"""
+for i in range(1, 10):
+    print("angka ke ", i)
